0aba970f4caaaa2d66319b90f622defa87a8a84f
yadan <wanyeonkim@gmail.com>
2020-11-28 17:54:58 +0800

    account book
    just a sketch

---
+++ BankingSystem/src/Account.java
@@ -0,0 +1,54 @@
+public class Account extends DataBase{
+    private String UserName;
+    private double Value;
+    private double InitialValue;
+    private String reason;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public double getValue() {
+        return Value;
+    }
+
+    public void setValue(double value) {
+        Value = value;
+    }
+
+    public double getInitialValue() {
+        return InitialValue;
+    }
+
+    public void setInitialValue(double initialValue) {
+        InitialValue = initialValue;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    Account(){
+
+    }
+
+    public void DepositAccount(String reason){
+        setInitialValue(getInitialValue()+getValue());
+        setReason(reason);
+    }
+
+    public void WithdrawAccount(String reason){
+        setInitialValue(getInitialValue()-getValue());
+        setReason(reason);
+    }
+
+
+}
---
+++ BankingSystem/src/DataBase.java
@@ -0,0 +1,9 @@
+import org.sqlite.core.DB;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.ArrayList;
+public class DataBase {
+
+}
